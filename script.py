97eac41787398627a27d5d0c0d3a539143d272a3
bolivars <102290605+bolivars@users.noreply.github.com>
2023-01-23 16:51:40 +0100

    Update script.py

--- script.py
+++ script.py
@@ -4,12 +4,11 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
-print("Number of arguments: %i arguments" % len(sys.arg))
+print("Number of arguments: %i arguments" % len(sys.argv))
 print("Argument List:" + str(sys.argv))
 
-data = pd.read_csv(sys.argv[1], delimiter=";")
-
-precipitation = data["y"]
+data = pd.read_csv("meteo.csv", delimiter=",")
+precipitation = data.iloc[:, 1]
 
 plt.plot(precipitation)
 plt.xlabel('Observation')
